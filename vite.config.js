253c86fc802d6813fd671453e72d2dc8adab854d
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-12-02 18:17:28 +0200

    initialize space shooter project with basic structure and configuration

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
+
+export default defineConfig({
+  build: {
+    target: "es2018",
+    outDir: "build",
+  },
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    hmr: true,
+  },
+  plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
+});
