717e393f77cea7a928dd9f7e4c030c2138c33996
klima3775 <113065590+klima3775@users.noreply.github.com>
2025-04-28 13:40:03 +0300

    segregation clases btn

--- src/classes/PauseMenu.js
+++ src/classes/PauseMenu.js
@@ -1,22 +1,19 @@
 import { Container, Graphics, Text } from "pixi.js";
+import { RestartButton } from "./RestartButton.js";
 
 export class PauseMenu {
   constructor(app, onResume, onRestart) {
     this.app = app;
-
-    // Контейнер для меню паузы
     this.container = new Container();
     this.container.visible = false;
     this.app.stage.addChild(this.container);
 
-    // Полупрозрачный фон
     const background = new Graphics();
     background.beginFill(0x000000, 0.7);
     background.drawRect(0, 0, this.app.view.width, this.app.view.height);
     background.endFill();
     this.container.addChild(background);
 
-    // Текст "PAUSED"
     const pauseTitle = new Text("PAUSED", {
       fontSize: 50,
       fill: 0xffffff,
@@ -26,7 +23,6 @@ export class PauseMenu {
     pauseTitle.y = this.app.view.height / 2 - 100;
     this.container.addChild(pauseTitle);
 
-    // Кнопка "Resume"
     const resumeText = new Text("Resume", {
       fontSize: 30,
       fill: 0xffffff,
@@ -36,28 +32,21 @@ export class PauseMenu {
     resumeText.y = this.app.view.height / 2;
     resumeText.interactive = true;
     resumeText.buttonMode = true;
-    resumeText.on("pointerdown", onResume); // Вызываем переданную функцию
+    resumeText.on("pointerdown", onResume);
     this.container.addChild(resumeText);
 
-    // Кнопка "Restart"
-    const restartText = new Text("Restart", {
-      fontSize: 30,
-      fill: 0xffffff,
-      align: "center",
-    });
-    restartText.x = this.app.view.width / 2 - restartText.width / 2;
-    restartText.y = this.app.view.height / 2 + 50;
-    restartText.interactive = true;
-    restartText.buttonMode = true;
-    restartText.on("pointerdown", onRestart); // Вызываем переданную функцию
-    this.container.addChild(restartText);
+    // Кнопка "Restart" через отдельный класс
+    this.restartButton = new RestartButton(this.app, onRestart);
+    this.container.addChild(this.restartButton.button);
   }
 
   show() {
     this.container.visible = true;
+    this.restartButton.show();
   }
 
   hide() {
     this.container.visible = false;
+    this.restartButton.hide();
   }
 }
---
+++ src/classes/RestartButton.js
@@ -0,0 +1,37 @@
+import { Text } from "pixi.js";
+
+export class RestartButton {
+  constructor(app, onRestart, x = null, y = null) {
+    this.app = app;
+    this.onRestart = onRestart;
+
+    this.button = new Text("Restart", {
+      fontSize: 30,
+      fill: 0xffffff,
+      align: "center",
+    });
+
+    // Если переданы координаты - ставим туда, иначе по центру
+    this.button.x = x !== null ? x : app.view.width / 2 - this.button.width / 2;
+    this.button.y = y !== null ? y : app.view.height / 2 + 50;
+
+    this.button.interactive = true;
+    this.button.buttonMode = true;
+    this.button.on("pointerdown", this.onRestart);
+
+    app.stage.addChild(this.button);
+  }
+
+  destroy() {
+    this.button.off("pointerdown", this.onRestart);
+    this.app.stage.removeChild(this.button);
+  }
+
+  hide() {
+    this.button.visible = false;
+  }
+
+  show() {
+    this.button.visible = true;
+  }
+}
