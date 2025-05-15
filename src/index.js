558be6cbb3b0b5b7fa2e3bb17b34d90d65b45005
klima3775 <113065590+klima3775@users.noreply.github.com>
2025-05-15 18:29:14 +0300

    add audio before starting game

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ document.getElementById("startButton").addEventListener("click", async () => {
   document.getElementById("background").remove();
 
   const app = new Application();
-
   await app.init({ width: 1600, height: 900 });
-
   document.body.appendChild(app.canvas);
 
   const assets = {
@@ -16,17 +14,15 @@ document.getElementById("startButton").addEventListener("click", async () => {
     player: "assets/spaceship.png",
     asteroid: "assets/asteroid.png",
   };
-
   Assets.addBundle("gameAssets", assets);
-
   const textures = await Assets.loadBundle("gameAssets");
 
-  const game = new Game(app, textures);
+  const game = new Game(app, textures); // Перенесено сюда
+  game.sounds.start.play(); // Проигрываем звук старта
 
   window.addEventListener("keydown", (event) => {
     if (event.key === "p" || event.key === "з") {
-      // Нажатие клавиши "P" для паузы
-      game.togglePause(); // Вызов метода togglePause из класса Game
+      game.togglePause();
     }
   });
 });
